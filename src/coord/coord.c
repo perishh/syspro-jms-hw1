1c1bbba7e553d93e997df9d20eaad098f3b7d9b2
Peris <vlassis.periklis@gmail.com>
2026-03-23 18:30:28 +0200

    coord: added argument parsing

--- src/coord/coord.c
+++ src/coord/coord.c
@@ -1,1 +1,33 @@
-int main() { return 1; }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void print_usage() { printf("Usage: jms_coord -l <path> -n <jobs_pool>"); }
+
+int main(int argc, char **argv) {
+  char *path = NULL;
+  int jobs_pool = 0;
+
+  // getopt(3)
+  int opt;
+  while ((opt = getopt(argc, argv, "l:n:")) != -1) {
+    switch (opt) {
+    case 'l':
+      path = optarg;
+      break;
+    case 'n':
+      jobs_pool = atoi(optarg);
+      break;
+    default:
+      // Empty or unknown argument
+      print_usage();
+      return 1;
+    }
+  }
+
+  if (path == NULL || jobs_pool <= 0) {
+    // Ensure arguments are valid
+    print_usage();
+    return 1;
+  }
+}
