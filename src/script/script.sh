94c59eb56b05f5df67db00c3e45c48c034e4dfa0
Peris <vlassis.periklis@gmail.com>
2026-04-22 19:14:15 +0300

    script: added command argument parsing

--- src/script/script.sh
+++ src/script/script.sh
@@ -2,12 +2,31 @@
 
 WORKDIR=""
 COMMAND=""
+SIZE=""
 
-while getopts "l:c:" opt; do
-  case $opt in
-    l) WORKDIR="$OPTARG" ;;
-    c) COMMAND="$OPTARG" ;;
-    *) echo "Unknown argument"; exit 1;;
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -l)
+      shift
+      WORKDIR="$1"
+      shift
+      ;;
+    -c)
+      shift
+      COMMAND="$1"
+      shift
+
+      # Read command argument if exists
+      if [[ $# -gt 0 && "$1" != -* ]]; then
+        SIZE="$1"
+        shift
+      fi
+      ;;
+    *)
+      # TODO: Print usage
+      echo "Invalid argument"
+      exit 1
+      ;;
   esac
 done
 
@@ -23,5 +42,10 @@ fi
 
 if [[ "$COMMAND" != "list" && "$COMMAND" != "size" && "$COMMAND" != "purge" ]]; then
   echo "Invalid command"
+  exit 1
+fi
+
+if [[ "$COMMAND" != "size" && -n "$SIZE" ]]; then
+  echo "Invalid argument"
   exit 1
 fi
