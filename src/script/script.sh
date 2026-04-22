3402190096ddcb98a0400e4a63997388e8bf2519
Peris <vlassis.periklis@gmail.com>
2026-04-22 15:26:02 +0300

    script: added basic argument parsing

--- src/script/script.sh
+++ src/script/script.sh
@@ -1,1 +1,27 @@
 #! /bin/bash
+
+WORKDIR=""
+COMMAND=""
+
+while getopts "l:c:" opt; do
+  case $opt in
+    l) WORKDIR="$OPTARG" ;;
+    c) COMMAND="$OPTARG" ;;
+    *) echo "Unknown argument"; exit 1;;
+  esac
+done
+
+if [[ -z "$WORKDIR" || -z "$COMMAND" ]]; then
+  echo "Invalid arguments"
+  exit 1
+fi
+
+if [[ ! -d "$WORKDIR" ]]; then
+  echo "Directory not found"
+  exit 1
+fi
+
+if [[ "$COMMAND" != "list" && "$COMMAND" != "size" && "$COMMAND" != "purge" ]]; then
+  echo "Invalid command"
+  exit 1
+fi
